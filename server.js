e4228c642aadd1b881c7892d48cd68ed700fc079
Darpit <darpit01@gmail.com>
2020-12-02 20:47:07 +0530

    changes in deployment files

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dap-commerce'));
+app.use(express.static(__dirname + '/dist/dap-commerce'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/dap-commerce/index.html'));
+res.sendFile(path.join(__dirname+'/dist/dap-commerce/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
